9fb66628307befce101f4b005c8a02a768709b8b
XTxiatong <2013301200055@whu.edu.cn>
2021-08-20 20:17:09 +0100

    Update prediction_via_inference.py

--- YAMNet/prediction_via_inference.py
+++ YAMNet/prediction_via_inference.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 20 14:49:15 2021
+
+@author: Jing Han
+
+Sound type identifcation from YAMNet inference
+""
+
 import numpy as np
 import csv
 import pandas as pd
